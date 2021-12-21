15bf1c1223c8ed21fd82be8200787296fe161ad8
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2021-12-21 22:51:49 +0900

    Initial commit

---
+++ app/src/main/java/com/beit_and_pear/android/viewample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.beit_and_pear.android.viewample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
